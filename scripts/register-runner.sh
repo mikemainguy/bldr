32b06950b4fdc34d73e05f1e6fa934e52cc99ea7
Michael Mainguy <mike.mainguy@gmail.com>
2025-07-03 13:18:39 -0500

    Enable shell tracing to log all commands and output to register-runner.log

--- scripts/register-runner.sh
+++ scripts/register-runner.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Log all output to a file in the current working directory
+# Log all commands and their output to a file in the current working directory
 LOGFILE="$(pwd)/register-runner.log"
 exec > >(tee -a "$LOGFILE") 2>&1
+set -x
 
 # GitHub Actions Runner Registration Script
 # This script registers a self-hosted runner with GitHub
